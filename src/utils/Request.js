27d85bfcc734b845e2d2788f9eaf9fed608ca278
JOKER XU <18696790771@163.com>
2018-04-09 20:10:03 +0800

    update Restful api method in Request until

--- src/utils/Request.js
+++ src/utils/Request.js
@@ -1,99 +1,133 @@
 import fetch from 'isomorphic-fetch';
 
-let BaseUrl = ''
+let BASE_URL = ''
 // development api
 if (process.env.NODE_ENV !== 'production') {
-  BaseUrl = 'http://192.168.1.29:8849';
+  BASE_URL = 'http://192.168.1.29:8849';
+}
+// content-type
+const ContentType = {
+  JSON: 'application/json; charset=UTF-8',
+  FORM: 'application/x-www-form-urlencoded; charset=UTF-8'
+}
+
+// Http method
+const HttpMethod = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  PATCH: 'PATCH',
+  DELETE: 'DELETE'
+}
+
+// common check
+const checkStatus = response => {
+  if (Object.is(200, response.status)) return response.json()
+  throw new Error(response.status);
+}
+
+// common error
+const handleError = promise => {
+  return promise
+    .then(response => checkStatus(response))
+    .catch((error) => {
+      // const { message: status } = error
+      // TODO you can handle the common error here
+      throw new Error('服务器发生未知错误');
+    });
+}
+
+// handle url
+const getUrl = (url) => {
+  if (url.startsWith('http://') || url.startsWith('https://')) return url
+  return `${BASE_URL}${url}`;
+}
+
+// token header
+export const getTokenHeaders = () => {
+  // TODO get the token permission request
+  // const token = Cookies.get(COOKIE_TOKEN)
+  const token = ''
+  return {
+    'Accept': ContentType.JSON,
+    'Content-Type': ContentType.JSON,
+    'Token': token
+  };
+}
+
+// common header
+const getHeaders = () => {
+  return {
+    'Accept': ContentType.JSON,
+    'Content-Type': ContentType.JSON
+  };
 }
 
 export default {
-  /**
-   * 基于 fetch 封装的 GET请求
-   * @param url
+  /** GET请求
+   * @param url: string
    * @param params {}
    * @param headers
-   * @returns {Promise}
+   * @returns Promise<any>
    */
-  get: function (url, params, headers) {
+  get: function (url, params, headers = getHeaders()) {
     if (params) {
-      const paramsArray = [];
-      //encodeURIComponent
-      Object.keys(params).forEach(key => paramsArray.push(`${key}=${params[key]}`));
+      const paramsArray = []
+      // encodeURIComponent
+      Object.keys(params).forEach(key => paramsArray.push(`${key}=${params[key]}`))
       if (url.search(/\?/) === -1) {
         url = `${url}?${paramsArray.join('&')}`;
       } else {
         url = `${url}&${paramsArray.join('&')}`;
       }
     }
-    return new Promise((resolve, reject) => {
-      fetch(`${BaseUrl}${url}`, {
-        method: 'GET',
-        headers: headers,
-      }).then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          reject(new Error({status: response.status}));
-        }
-      }).then((response) => {
-        resolve(response);
-      }).catch((err) => {
-        reject(new Error({status: -1}));
-      });
-    });
+    const promise = fetch(getUrl(url), {
+      method: HttpMethod.GET,
+      headers: headers
+    })
+    return handleError(promise);
   },
-  /**
-   * 基于 fetch 封装的 POST请求  FormData 表单数据
+  /** POST请求  FormData 表单数据
    * @param url
    * @param formData
    * @param headers
-   * @returns {Promise}
+   * @returns Promise<any>
    */
-  postForm: function (url, formData, headers) {
-    return new Promise(function (resolve, reject) {
-      fetch(`${BaseUrl}url`, {
-        method: 'POST',
-        headers: headers,
-        body: formData,
-      }).then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          reject(new Error({status: response.status}));
-        }
-      }).then((response) => {
-        resolve(response);
-      }).catch((err) => {
-        reject(new Error({status: -1}));
-      });
-    });
+  postForm: function (url, formData, headers = getHeaders()) {
+    const promise = fetch(getUrl(url), {
+      method: HttpMethod.POST,
+      headers: headers,
+      body: formData
+    })
+    return handleError(promise);
   },
-  /***
-   *  post请求
-   *  url:请求地址
-   *  params:参数,这里的参数格式是：{param1: 'value1',param2: 'value2'}
+  /*** POST 请求
+   * @param url
+   * @param params 参数,这里的参数格式是：{param1: 'value1',param2: 'value2'}
+   * @param headers
+   * @returns Promise<any>
    * */
-  postJSON: function (url, params) {
-    return new Promise((resolve, reject) => {
-      fetch(`${BaseUrl}url`, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(params)
-      }).then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          reject(new Error({status: response.status}));
-        }
-      }).then((response) => {
-        resolve(response);
-      }).catch((err) => {
-        reject(new Error({status: -1}));
-      });
-    });
-  }
+  postJSON: function (url, params, headers = getHeaders()) {
+    const promise = fetch(getUrl(url), {
+      method: HttpMethod.POST,
+      headers: headers,
+      body: JSON.stringify(params)
+    })
+    return handleError(promise);
+  },
+  /*** PATCH 请求
+   * @param url
+   * @param params 参数,这里的参数格式是：{param1: 'value1',param2: 'value2'}
+   * @param headers
+   * @returns Promise<any>
+   * */
+  patchJSON: function (url, params, headers = getHeaders()) {
+    const promise = fetch(getUrl(url), {
+      method: HttpMethod.PATCH,
+      headers: headers,
+      body: JSON.stringify(params)
+    })
+    return handleError(promise);
+  },
 };
 
